c16df6e5df3c41fa48df57271861adab786d3733
Dave Roberts <d.roberts@ctrlo.com>
2025-07-01 13:06:07 +0100

    Initial project

--- src/main/kotlin/blog/davetheitguy/tiqqet/backend/TiqqetBackendApplication.kt
+++ src/main/kotlin/blog/davetheitguy/tiqqet/backend/TiqqetBackendApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class TiqqetBackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<TiqqetBackendApplication>(*args)
+    runApplication<TiqqetBackendApplication>(*args)
 }
